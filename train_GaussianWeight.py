Fix loss calculation in train function

train_GaussianWeight.py
@@ -103,6 +103,7 @@ def train(model_name, model_path, model_params, timestamp, **kwargs):
     # Define loss function based on supervised or unsupervised learning
     # criterion = model_params.loss_image
     # criterion = nn.MSELoss()
+    print(**kwargs)
     criterion = GaussianWeightedMSELoss(**kwargs)
     extra = loss_extra()
     criterion_points = nn.MSELoss() # 
@@ -183,8 +184,8 @@ def train(model_name, model_path, model_params, timestamp, **kwargs):
             # print(source_image.shape, target_image.shape, affine_params_true.shape,
             #     points1.shape, points2.shape, points1_2_true.shape)
 
-            source_image = source_image.requires_grad_(True).to(device)
-            target_image = target_image.requires_grad_(True).to(device)
+            source_image = source_image.to(device)
+            target_image = target_image.to(device)
             # add gradient to the matches
             # points1.requires_grad_(True).to(device)
             # points2.requires_grad_(True).to(device)

utils/train.py
@@ -163,6 +163,7 @@ def train(model_name, model_path, model_params, timestamp):
         model.eval()
         with torch.no_grad():
             for i, data in enumerate(test_dataset, 0):
+                loss = 0.0
                 # Get images and affine parameters
                 if model_params.sup:
                     source_image, target_image, affine_params_true = data
@@ -192,7 +193,7 @@ def train(model_name, model_path, model_params, timestamp):
                 heatmap1 = outputs[7]
                 heatmap2 = outputs[8]
 
-                loss = criterion(transformed_source_affine, target_image)
+                loss += criterion(transformed_source_affine, target_image)
                 # loss += extra(affine_params_predicted)
                 if model_params.sup:
                     loss_affine = criterion_affine(affine_params_true.view(1, 2, 3), affine_params_predicted.cpu())