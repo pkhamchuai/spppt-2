1876409f4ca96d8e474af371ebfd1f4b0f218db2
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-01-25 11:44:57 +0700

    update train batch

--- train_batch.py
+++ train_batch.py
@@ -1,18 +1,26 @@
 import subprocess
 
-dataset = [1, 2, 0]
-sups = [1, 1, 0]
-# models = ['DHR', 'SP_AffineNet']
+dataset = [0, 1, 2, 3]
+models = ['DHR']
 # models = ['SP_AffineNet4', 'DHR']
+sups = [1, 0]
 runs = []
-learning_rate = 1e-4
-loss = [[1, 1]]
-offset = 0.01
-sigma = range(30, 100, 10)
+# learning_rate = 1e-4
+# loss = [[1, 1]]
+# offset = 0.01
+# sigma = range(30, 100, 10)
+
 # generate run commands
-for j in sigma:
+for j in sups:
+    for i in dataset:
+        if i == 0 and j == 1:
+            pass
+        else:
+            runs.append(['python', 'train.py', '--dataset', str(dataset[i]), '--model', 'DHR', 
+                         '--supervised', str(j)])
     
 
 for i in range(len(runs)):
-    print(runs[i])
-    # subprocess.run(runs[i])
+    for j in range(3):
+        print(runs[i])
+        # subprocess.run(runs[i])
