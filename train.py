8262c5d2ddc6d6e519cab0c40e6276c62294f033
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-02-14 16:34:09 +0700

    Refactor training script to include new models and support different supervision levels

--- train.py
+++ train.py
@@ -11,24 +11,24 @@ runs = []
 # sigma = range(30, 100, 10)
 
 # generate run commands
-# for sup in sups:
 for model in models:
-    # for data in dataset:
-    #     if data == 0 and sup == 1:
-    #         pass
-    #     else:
-            # runs.append(['python', 'train_points_rigid.py', '--dataset', str(data), 
-            #              '--model', 'DHR', 
-            #              '--image', str(1), '--points', str(0), '--sup', str(sup)])
-            # runs.append(['python', 'train_one_sample.py', '--dataset', str(data), 
-            #              '--model', 'DHR', '--num_epochs', str(100), 
-            #              '--image', str(1), '--points', str(0), '--sup', str(sup)])
-    runs.append(['python', 'train_img_batch.py', '--dataset', str(3), 
-                            '--model', str(model), '--num_epochs', str(10), 
-                            '--image', str(1), '--points', str(0), '--sup', str(1)])
+    for sup in sups:    
+        # for data in dataset:
+        if model != 'AIRNet' and sup == 0:
+                pass
+        else:
+                # runs.append(['python', 'train_points_rigid.py', '--dataset', str(data), 
+                #              '--model', 'DHR', 
+                #              '--image', str(1), '--points', str(0), '--sup', str(sup)])
+                # runs.append(['python', 'train_one_sample.py', '--dataset', str(data), 
+                #              '--model', 'DHR', '--num_epochs', str(100), 
+                #              '--image', str(1), '--points', str(0), '--sup', str(sup)])
+            runs.append(['python', 'train_img_batch.py', '--dataset', str(3), 
+                                '--model', str(model), '--num_epochs', str(10), 
+                                '--image', str(1), '--points', str(0), '--sup', str(sup)])
     
 
 for i in range(len(runs)):
-    for j in range(1):
+    for j in range(3):
         print(runs[i])
         subprocess.run(runs[i])
