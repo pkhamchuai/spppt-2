697d210e2ccbb38c6219c550f39847261686ab5c
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-02-21 22:55:58 +0700

    Refactor SP_DHR_Net class in SP_DHR_Attn.py

--- utils/SP_DHR_Attn.py
+++ utils/SP_DHR_Attn.py
@@ -15,27 +15,30 @@ from networks import affine_network_attention as an
 from utils.SuperPoint import SuperPointFrontend
 from utils.utils1 import transform_points_DVF
 
-class SP_DHR_Net(nn.Module):
-    def __init__(self, model_params):
-        super(SP_DHR_Net, self).__init__()
-        self.affineNet = an.load_network(device)
-        self.model_params = model_params
-        print("\nRunning new version (not run SP on source image)")
 
-        # inputs = torch.rand((1, 1, image_size, image_size)), torch.rand((1, 1, image_size, image_size))
-        # summary(self.affineNet, *inputs, show_input=True, show_hierarchical=True, print_summary=True)
 
-    def forward(self, source_image, target_image, points):
-        # if self.model_params.points == 1:
-        affine_params = self.affineNet(source_image, target_image)
+# below is not real attention model
+# class SP_DHR_Net(nn.Module):
+#     def __init__(self, model_params):
+#         super(SP_DHR_Net, self).__init__()
+#         self.affineNet = an.load_network(device)
+#         self.model_params = model_params
+#         print("\nRunning new version (not run SP on source image)")
+
+#         # inputs = torch.rand((1, 1, image_size, image_size)), torch.rand((1, 1, image_size, image_size))
+#         # summary(self.affineNet, *inputs, show_input=True, show_hierarchical=True, print_summary=True)
+
+#     def forward(self, source_image, target_image, points):
+#         # if self.model_params.points == 1:
+#         affine_params = self.affineNet(source_image, target_image)
         
-        # elif self.model_params.heatmaps == 1:
-        #     print("This part is not yet implemented.")
-            # affine_params = self.affineNet(source_image, target_image, heatmap1, heatmap2)
-        transformed_source_image = tensor_affine_transform(source_image, affine_params)
-        transformed_points = points.clone()
-        transformed_points = transform_points_DVF(transformed_points[0].cpu().detach().T, 
-            affine_params.cpu().detach(), transformed_source_image.cpu().detach())
+#         # elif self.model_params.heatmaps == 1:
+#         #     print("This part is not yet implemented.")
+#             # affine_params = self.affineNet(source_image, target_image, heatmap1, heatmap2)
+#         transformed_source_image = tensor_affine_transform(source_image, affine_params)
+#         transformed_points = points.clone()
+#         transformed_points = transform_points_DVF(transformed_points[0].cpu().detach().T, 
+#             affine_params.cpu().detach(), transformed_source_image.cpu().detach())
 
-        return transformed_source_image, affine_params, transformed_points.T
+#         return transformed_source_image, affine_params, transformed_points.T
     
