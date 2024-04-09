67aaa56dc424617b696e9af090666587d1a80b47
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-04-09 23:33:17 +0700

    Refactor model_path variable in test.py and train.py, update datasets in train.py and test.py, and refactor import statements

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 
 # dataset = range(0, 6)
 # sups = [1, 1, 1, 1, 1, 0]
-dataset = [1]
+dataset = [5]
 sups = [1]
 models = ['DHR']
 # models = ['Attention']
@@ -31,7 +31,7 @@ files = os.listdir('trained_models/')
 #               '20240403-172755_Attention_stage4_00100_0.001_15_20_1.pth', # wo Avg 4530
 # model_path = ['Attention_11100_0.001_0_10_50_20240409-053141.pth',
 #               'Attention_21100_0.001_0_10_50_20240409-054404.pth']
-model_path = ['DHR_41100_0.001_0_10_50_20240409-125816.pth']
+model_path = ['DHR_51100_0.001_0_10_100_20240409-152401.pth']
 print(model_path)
 
 runs = []
@@ -43,15 +43,15 @@ for model, dataset_ in zip(model_path, dataset):
         #              '--dataset', str(dataset_),
         #             '--model_path', str(model)
         #                         ])
-        runs.append(['python', 'test_two_ways.py', '--model', str('Attention'), '--sup', str(1),
+        runs.append(['python', 'test_two_ways.py', '--model', str('DHR'), '--sup', str(1),
                      '--dataset', str(dataset_),
                     '--model_path', str(model)
                                 ])
-        runs.append(['python', 'test_rep1.py', '--model', str('Attention'), '--sup', str(1),
+        runs.append(['python', 'test_rep1.py', '--model', str('DHR'), '--sup', str(1),
                      '--dataset', str(dataset_),
                     '--model_path', str(model)
                                 ])
-        runs.append(['python', 'test_rep2.py', '--model', str('Attention'), '--sup', str(1),
+        runs.append(['python', 'test_rep2.py', '--model', str('DHR'), '--sup', str(1),
                      '--dataset', str(dataset_),
                     '--model_path', str(model)
                                 ])
