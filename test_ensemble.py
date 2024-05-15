a6ae4aae30e7ced8137418a51fa387c18a8f164d
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-05-15 14:32:24 +0700

    chore: Update test_ensemble.py to use test_ensemble_2way.py for ensemble testing

--- test_ensemble.py
+++ test_ensemble.py
@@ -64,8 +64,14 @@ runs = []
 learning_rate = 1e-3
 
 # generate run commands
+# for dataset_, sup in zip(dataset, sups):
+#     runs.append(['python', 'test_ensemble_1way.py', '--model', str(models), '--sup', str(sup),
+#                      '--dataset', str(dataset_),
+#                     '--model_path', str(model_path), '--plot', '1'
+#                                 ])
+    
 for dataset_, sup in zip(dataset, sups):
-    runs.append(['python', 'test_ensemble_1way.py', '--model', str(models), '--sup', str(sup),
+    runs.append(['python', 'test_ensemble_2way.py', '--model', str(models), '--sup', str(sup),
                      '--dataset', str(dataset_),
                     '--model_path', str(model_path), '--plot', '1'
                                 ])
