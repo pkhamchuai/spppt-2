0b3f1533d8d0755d1f6d6673058b485bf9ac0baa
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-06-12 23:37:29 +0700

    Refactor dataset range and model name in train.py, update SIFT code in run_SIFT.py, and modify Rep1

--- test.py
+++ test.py
@@ -43,11 +43,11 @@ files = os.listdir('trained_models/')
 # model_name = 'DHRoriginal'
 # model_path = ['DHRoriginal_51100_0.001_0_10_100_20240530-142556.pth']
 
-model_name = 'DHRdiff'
-model_path = ['DHR_51100_0.001_0_5_100_20240509-140837.pth']
+# model_name = 'DHRdiff'
+# model_path = ['DHR_51100_0.001_0_5_100_20240509-140837.pth']
 
-# model_name = 'DHR2x'
-# model_path = ['DHR2x_51100_0.001_0_5_100_20240513-112002.pth']
+model_name = 'DHR2x'
+model_path = ['DHR2x_51100_0.001_0_5_100_20240513-112002.pth']
 
 # model_path = ['DHR_11100_0.001_0_5_100_20240509-155916.pth', 'DHR_21100_0.001_0_5_100_20240509-160207.pth',
 #               'DHR_31100_0.001_0_10_100_20240508-120807.pth', 'DHR_41100_0.001_0_5_100_20240509-133824.pth',
@@ -60,14 +60,15 @@ learning_rate = 1e-3
 # generate run commands
 for model in model_path:
     for dataset_, sup in zip(dataset, sups):
-        # runs.append(['python', 'test_points.py', '--model', str(model_name), '--sup', str(sup),
-        #              '--dataset', str(dataset_),
-        #             '--model_path', str(model), '--plot', '1'
-        #                         ])
-        # runs.append(['python', 'test_points.py', '--model', str(model_name), '--sup', str(sup),
-        #              '--dataset', str(dataset_),
-        #             '--model_path', str(model), '--plot', '2'
-        #                         ])
+        runs.append(['python', 'test_points.py', '--model', str(model_name), '--sup', str(sup),
+                     '--dataset', str(dataset_),
+                    '--model_path', str(model), '--plot', '1'
+                                ])
+        runs.append(['python', 'test_points.py', '--model', str(model_name), '--sup', str(sup),
+                     '--dataset', str(dataset_),
+                    '--model_path', str(model), '--plot', '2'
+                                ])
+        
         # runs.append(['python', 'test_two_ways.py', '--model', str(model_name), '--sup', str(sup),
         #             '--dataset', str(dataset_),
         #             '--model_path', str(model)
@@ -77,6 +78,7 @@ for model in model_path:
         #              '--dataset', str(dataset_),
         #             '--model_path', str(model)
         #                         ])
+        
         runs.append(['python', 'test_rep1.py', '--model', str(model_name), '--sup', str(sup),
                     '--dataset', str(dataset_),
                     '--model_path', str(model), '--plot', '1'
