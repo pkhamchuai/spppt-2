7f03c8a85f015a13258b40ed372552362b9ebf32
pkhamchuai <pakpoom_kh@cmu.ac.th>
2024-10-06 08:22:29 +0700

    Refactor test_BCS_DHRdiff.py to update dataset and sups values

--- test_BCS_DHRdiff.py
+++ test_BCS_DHRdiff.py
@@ -3,11 +3,11 @@ import os
 
 # dataset = range(0, 6)
 # sups = [0, 1, 1, 1, 1, 1]
-dataset = range(10, 13)
+# dataset = range(10, 13)
 # sups = [1, 1, 1, 1, 1]
 # dataset = [4, 5]
 # sups = [1, 1]
-# dataset = [3]
+dataset = [3]
 sups = [0]
 
 # models = ['Attention']
@@ -104,22 +104,22 @@ learning_rate = 1e-3
 #                      '--dataset', str(dataset_),
 #                     '--model_path', str(model_path), '--plot', '1', '--verbose', '0'
 #                                 ])
-# for dataset_ in dataset:
-#     runs.append(['python', 'test_BCS_1way.py', '--model', str(models), '--sup', str(0),
-#                      '--dataset', str(dataset_), '--beam', '1',
-#                     '--model_path', str(model_path), '--plot', '1', '--verbose', '0'
-#                                 ])
+for dataset_ in dataset:
+    runs.append(['python', 'test_BCS_1way.py', '--model', str(models), '--sup', str(0),
+                     '--dataset', str(dataset_), '--beam', '1',
+                    '--model_path', str(model_path), '--plot', '1', '--verbose', '1'
+                                ])
     
-# for dataset_ in dataset:
-#     runs.append(['python', 'test_BCS_1way.py', '--model', str(models), '--sup', str(0),
-#                      '--dataset', str(dataset_), '--beam', '2',
-#                     '--model_path', str(model_path), '--plot', '1', '--verbose', '0'
-#                                 ])
+for dataset_ in dataset:
+    runs.append(['python', 'test_BCS_1way.py', '--model', str(models), '--sup', str(0),
+                     '--dataset', str(dataset_), '--beam', '2',
+                    '--model_path', str(model_path), '--plot', '1', '--verbose', '1'
+                                ])
     
 for dataset_ in dataset:
     runs.append(['python', 'test_BCS_1way.py', '--model', str(models), '--sup', str(0),
                      '--dataset', str(dataset_), '--beam', '3',
-                    '--model_path', str(model_path), '--plot', '1', '--verbose', '0'
+                    '--model_path', str(model_path), '--plot', '1', '--verbose', '1'
                                 ])
         
 # sort runs by element 1, then 11, then 7
