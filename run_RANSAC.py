TODO: revise RANSAC code, fix error per iteration calculation

run_RANSAC.py
@@ -78,50 +78,29 @@ def run(model_params):
         # process images
         source_image = process_image(source_image)
         target_image = process_image(target_image)
+        points1 = points1[0].cpu().numpy()
+        points2 = points2[0].cpu().numpy()
 
-        # Process the first image
-        points1, desc1, heatmap1 = superpoint(source_image)
-        # Process the second image
-        points2, desc2, heatmap2 = superpoint(target_image)
+        print(points1.shape, points2.shape)
 
-        # match the points between the two images
-        tracker = PointTracker(5, nn_thresh=0.7)
-        matches = tracker.nn_match_two_way(desc1, desc2, nn_thresh=0.7)
+        affine_transform1 = cv2.estimateAffinePartial2D(points1, points2, method=cv2.RANSAC)
+        matches1_transformed = cv2.transform(points1, affine_transform1[0])
+        matches1_transformed = matches1_transformed[0].T
+        # transform image 1 and 2 using the affine transform matrix
+        transformed_source_affine = cv2.warpAffine(source_image, affine_transform1[0], (256, 256))
 
-        # take the elements from points1 and points2 using the matches as indices
-        matches1 = points1[:2, matches[0, :].astype(int)]
-        matches2 = points2[:2, matches[1, :].astype(int)]
-
-        matches_RANSAC = tracker.ransac(matches1.T, matches2.T, matches, max_reproj_error=2)
-            
-        # take elements from matches1 and matches2 where matches_RANSAC is TRUE
-        matches_RANSAC = matches_RANSAC.reshape(-1)
-        matches1_RANSAC = matches1[:, matches_RANSAC]
-        matches2_RANSAC = matches2[:, matches_RANSAC]
-
-        # # create affine transform matrix from points1 to points2
-        # # and apply it to points1
-        # affine_transform1 = cv2.estimateAffinePartial2D(matches1_RANSAC.T, matches2_RANSAC.T)
-        # matches1_transformed = cv2.transform(matches1.T[None, :, :], affine_transform1[0])
-        # matches1_transformed = matches1_transformed[0].T
-
-        # # transform image 1 and 2 using the affine transform matrix
-        # transformed_source_affine = cv2.warpAffine(source_image, affine_transform1[0], (256, 256))
-
-        # create affine transform matrix from points1 to points2
-        # and apply it to points1
-        try:
-            affine_transform1 = cv2.estimateAffinePartial2D(matches1_RANSAC.T, matches2_RANSAC.T)
-            matches1_transformed = cv2.transform(matches1.T[None, :, :], affine_transform1[0])
-            matches1_transformed = matches1_transformed[0].T
-            # transform image 1 and 2 using the affine transform matrix
-            transformed_source_affine = cv2.warpAffine(source_image, affine_transform1[0], (256, 256))
-        except cv2.error:
-            print(f"Error: {i}")
-            # set affine_transform1 to identity affine matrix
-            affine_transform1 = np.array([[[1, 0, 0], [0, 1, 0]]])
-            matches1_transformed = matches1
-            transformed_source_affine = source_image
+        # try:
+        #     affine_transform1 = cv2.estimateAffinePartial2D(points1.T, points2.T, method=cv2.RANSAC)
+        #     matches1_transformed = cv2.transform(points1[0], affine_transform1[0])
+        #     matches1_transformed = matches1_transformed[0].T
+        #     # transform image 1 and 2 using the affine transform matrix
+        #     transformed_source_affine = cv2.warpAffine(source_image, affine_transform1[0], (256, 256))
+        # except cv2.error:
+        #     print(f"Error: {i}")
+        #     # set affine_transform1 to identity affine matrix
+        #     affine_transform1 = np.array([[[1, 0, 0], [0, 1, 0]]])
+        #     matches1_transformed = points1
+        #     transformed_source_affine = source_image
 
         # mse12 = np.mean((matches1_transformed - matches2_RANSAC)**2)
         # tre12 = np.mean(np.sqrt(np.sum((matches1_transformed - matches2_RANSAC)**2, axis=0)))
@@ -134,10 +113,10 @@ def run(model_params):
         results = DL_affine_plot(f"test_{i+1}", output_dir,
                 f"{i}", "RANSAC", source_image, target_image, \
                 transformed_source_affine, \
-                matches1, matches2, matches1_transformed, desc1, desc2, 
+                points1, points2, points1_2_true, \
                 affine_params_true=affine_params_true,
                 affine_params_predict=affine_transform1[0], 
-                heatmap1=heatmap1, heatmap2=heatmap2, plot=plot_)
+                heatmap1=None, heatmap2=None, plot=plot_)
 
 
         # calculate metrics

test_BCS_1way_point_reverse.py
@@ -459,12 +459,7 @@ def test(model_name, models, model_params, timestamp,
                     outputs = model[model_number](source_image, target_image, points=points1)
                     affine_params_predicted = outputs[1]
                     M = combine_matrices(M, affine_params_predicted).to(device)
-
-                    # store error of each iteration
-                    error_img[i, k] = mse(outputs[0][0, 0, :, :].cpu().detach().numpy(), 
-                                        target_image[0, 0, :, :].cpu().detach().numpy())
-                    error_pt[i, k] = tre(outputs[2].cpu().detach().numpy().T, 
-                                        points2.cpu().detach().numpy())
+                    
                 else:
                     model_number = model_number - len(models)
                     outputs = model[model_number](target_image, source_image, points=points2)
@@ -476,16 +471,15 @@ def test(model_name, models, model_params, timestamp,
                     points1_2_predicted = transform_points_DVF(points1_0.cpu().detach().T,
                         M.cpu().detach(), source_image0).T
 
-                    # store error of each iteration
-                    error_img[i, k] = mse(transformed_source_affine[0, 0, :, :].cpu().detach().numpy(),
-                                        target_image[0, 0, :, :].cpu().detach().numpy())
-                    error_pt[i, k] = tre(points1_2_predicted.cpu().detach().numpy().T,
-                                        points2.cpu().detach().numpy())
-
                 if k == len(active_beams)-1:
                     transformed_source_affine = tensor_affine_transform0(source_image0, M)
                     points1_2_predicted = transform_points_DVF(points1_0.cpu().detach().T,
                                 M.cpu().detach(), source_image0).T
+                    # store error of each iteration
+                    error_img[i, k] = mse(outputs[0][0, 0, :, :].cpu().detach().numpy(), 
+                                        target_image[0, 0, :, :].cpu().detach().numpy())
+                    error_pt[i, k] = tre(outputs[2].cpu().detach().numpy().T, 
+                                        points2.cpu().detach().numpy())
                 else:
                     source_image = tensor_affine_transform0(source_image0, M)
                     points1 = transform_points_DVF(points1_0.cpu().detach().T,
@@ -559,12 +553,12 @@ def test(model_name, models, model_params, timestamp,
         writer.writerow(std)
 
     # replace the zeros with values from the last non-zero element
-    for i in range(len(error_img)):
-        for j in range(len(error_img[i])):
-            if error_img[i, j] == 0:
-                error_img[i, j] = error_img[i, j-1]
-            if error_pt[i, j] == 0:
-                error_pt[i, j] = error_pt[i, j-1]
+    # for i in range(len(error_img)):
+    #     for j in range(len(error_img[i])):
+    #         if error_img[i, j] == 0:
+    #             error_img[i, j] = error_img[i, j-1]
+    #         if error_pt[i, j] == 0:
+    #             error_pt[i, j] = error_pt[i, j-1]
 
     # calculate the average error per iteration and save in a new csv file
     error_img = error_img[~np.all(error_img == 0, axis=1)]
@@ -572,6 +566,9 @@ def test(model_name, models, model_params, timestamp,
 
     error_pt = error_pt[~np.all(error_pt == 0, axis=1)]
     error_pt_avg = np.mean(error_pt, axis=0)
+
+    print(error_pt)
+    print(error_pt_avg)
 
     csv_file = f"{output_dir}/error_{timestamp}.csv"
     with open(csv_file, 'w', newline='') as file: